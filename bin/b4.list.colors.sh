59f7e195d2d2a885c6fa6949cf59c515b32aab67
Thiago Gouveia <thiago.gouveia.da.silva@gmail.com>
2019-06-27 07:44:38 -0300

    --brief and --help on list.colors

--- bin/b4.list.colors.sh
+++ bin/b4.list.colors.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+opt=$1
+
+if [ "$opt" == "--help" ]; then
+	echo "Lists color names and hexadecimal values"
+	echo "   *** Usage: $0 [--help|--brief]"
+	exit 0
+fi
+
 # color list as a declarative array
 declare -A cl
 
@@ -13,30 +21,30 @@ add green 00FF00
 add black 000000
 add white 00FF00
 add pink FFC0CB
-add deeppink FF1493
+[ "$opt" != "--brief" ] && add deeppink FF1493
 add orange FFA500
-add orangered FF4500
-add gold FFD700
+[ "$opt" != "--brief" ] && add orangered FF4500
+[ "$opt" != "--brief" ] && add gold FFD700
 add yellow FFFF00
-add fuchsia FF00FF
+[ "$opt" != "--brief" ] && add fuchsia FF00FF
 add purple 800080
-add darkmagenta 8B008B
-add indigo 4B0082
+[ "$opt" != "--brief" ] && add darkmagenta 8B008B
+[ "$opt" != "--brief" ] && add indigo 4B0082
 add darkgreen 008000
-add lightgreen 7FFF00
+[ "$opt" != "--brief" ] && add lightgreen 7FFF00
 add cyan 00FFFF
-add steelblue 4682B4
-add deepskyblue 00BFFF
+[ "$opt" != "--brief" ] && add steelblue 4682B4
+[ "$opt" != "--brief" ] && add deepskyblue 00BFFF
 add darkblue 00008B
-add midnightblue 191970
-add goldenrod DAA520
-add brown 8B4513
-add azure F0FFFF
-add silver C0C0C0
+[ "$opt" != "--brief" ] && add midnightblue 191970
+[ "$opt" != "--brief" ] && add goldenrod DAA520
+[ "$opt" != "--brief" ] && add brown 8B4513
+[ "$opt" != "--brief" ] && add azure F0FFFF
+[ "$opt" != "--brief" ] && add silver C0C0C0
 add gray 808080
-add darkgray A9A9A9
-add dimgray 696969
-add lightgray d3d3d3
+[ "$opt" != "--brief" ] && add darkgray A9A9A9
+[ "$opt" != "--brief" ] && add dimgray 696969
+[ "$opt" != "--brief" ] && add lightgray d3d3d3
 
 
 # Iterate on the keys of cl
